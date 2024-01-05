f6be8b5b0c6a7c20e73fb6bb494c1fb93e3eb010
username <wjdwntjd55@naver.com>
2024-01-05 18:57:37 +0900

    feat : 회원탈퇴 화면 UI 구현 #15

--- app/src/main/java/com/project/comepethome/ui/mypage/MyPageModifyFragment.kt
+++ app/src/main/java/com/project/comepethome/ui/mypage/MyPageModifyFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 import com.project.comepethome.R
 import com.project.comepethome.databinding.FragmentMyPageModifyBinding
+import com.project.comepethome.databinding.ItemUserWithdrawBinding
 import com.project.comepethome.ui.main.MainActivity
 
 class MyPageModifyFragment : Fragment() {
@@ -51,6 +53,27 @@ class MyPageModifyFragment : Fragment() {
             textViewLogoutMyPageModify.setOnClickListener {
                 mainActivity.replaceFragment(MainActivity.LOG_IN_FRAGMENT, false, null)
                 Snackbar.make(binding.root, "로그아웃 되었습니다.", Snackbar.LENGTH_SHORT).show()
+            }
+
+            // 회원탈퇴
+            textViewWithdrawalMyPageModify.setOnClickListener {
+                val itemUserWithdrawBinding = ItemUserWithdrawBinding.inflate(layoutInflater)
+                val builder = MaterialAlertDialogBuilder(mainActivity)
+                builder.setView(itemUserWithdrawBinding.root)
+                val dialog = builder.create()
+
+                itemUserWithdrawBinding.buttonCancel.setOnClickListener {
+                    dialog.dismiss()
+                }
+
+                itemUserWithdrawBinding.buttonWithdraw.setOnClickListener {
+                    dialog.dismiss()
+                    mainActivity.removeFragment(MainActivity.MYPAGE_MODIFY_FRAGMENT)
+                    mainActivity.replaceFragment(MainActivity.LOG_IN_FRAGMENT, false, null)
+                    Snackbar.make(binding.root, "회원탈퇴 되었습니다.", Snackbar.LENGTH_SHORT).show()
+                }
+
+                dialog.show()
             }
 
         }
