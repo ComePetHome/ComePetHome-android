14c4a8a7e3e5d87fb83676992de857cb656963b4
username <wjdwntjd55@naver.com>
2024-01-10 16:58:07 +0900

    design : 회원가입 화면 UI - > 아아디 입력 하는 화면 UI 로 변경 #2

--- app/src/main/java/com/project/comepethome/ui/join/JoinIDFragment.kt
+++ app/src/main/java/com/project/comepethome/ui/join/JoinIDFragment.kt
@@ -1,10 +1,13 @@
 package com.project.comepethome.ui.join
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.project.comepethome.R
 import com.project.comepethome.databinding.FragmentJoinIdBinding
 import com.project.comepethome.ui.main.MainActivity
 
@@ -22,20 +25,48 @@ class JoinIDFragment : Fragment() {
         binding = FragmentJoinIdBinding.inflate(layoutInflater)
 
         closeButton()
+        enterID()
         joinButton()
 
         return binding.root
     }
 
     private fun closeButton() {
-        binding.imageCloseJoin.setOnClickListener {
+        binding.imageCloseJoinId.setOnClickListener {
             mainActivity.removeFragment(MainActivity.JOIN_ID_FRAGMENT)
         }
     }
 
+    private fun enterID() {
+        val idEditText = binding.editTextIdJoinId
+        val nextButton = binding.buttonNextJoinId
+
+        idEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // No implementation needed
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // No implementation needed
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Check if the EditText has text
+                val isIdEntered = s?.isNotEmpty() ?: false
+
+                // Set the background color of the button based on whether the EditText has text
+                nextButton.setBackgroundResource(
+                    if (isIdEntered) R.drawable.bg_rect_orange_r10
+                    else R.drawable.bg_rect_gray200_r10
+                )
+            }
+        })
+
+    }
+
     private fun joinButton() {
-        binding.buttonJoin.setOnClickListener {
-            mainActivity.replaceFragment(MainActivity.LOG_IN_FRAGMENT, false, null)
+        binding.buttonNextJoinId.setOnClickListener {
+            
         }
     }
 
